f6583d24cc66233d93993058476bb5361e87611c
goranje <goranje@phoenixnap.com>
2024-10-03 14:48:53 +0200

    documentation for GPU configuration added

--- plugins/modules/server_reserve.py
+++ plugins/modules/server_reserve.py
@@ -378,6 +378,18 @@ server_reserve:
               size:
                 description: The size of the root partition in GB. -1 to use all available space.
                 type: int
+      gpuConfiguration:
+        description: The GPU configuration.
+        type: dict
+        contains:
+          longName:
+            description: The long name of the GPU.
+            type: str
+            sample: Intel Max 1100 GPU
+          count:
+            description: The number of GPUs.
+            type: int
+            sample: 2
       supersededBy:
         description: Unique identifier of the server to which the reservation has been
         type: str
